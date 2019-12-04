948f4cf7e83b957af5edbcb47fdb913b292711df
ElektroTekniskaFöreningen Administratör <sekreterare@etf.nu>
2019-12-04 15:06:48 +0100

    Upgrade to CAS 2.0

--- koseki/plugins/cas.py
+++ koseki/plugins/cas.py
@@ -1,5 +1,6 @@
 from koseki import app, storage
 from flask import url_for, render_template, session, redirect, escape, request
+from xml.etree import ElementTree as ET
 from koseki.core import start_session, alternate_login
 from koseki.db.types import Person
 
@@ -9,7 +10,7 @@ import urllib.request, urllib.parse, urllib.error
 def cas_login():
     return {'text': 'Please sign in using CAS '+
             'if you are a student or employee at Lund University.',
-            'url': app.config['CAS_SERVER']+'/cas/login?service='+app.config['URL_BASE']+'/cas&renew=true',
+            'url': app.config['CAS_SERVER']+'/cas/login?service='+app.config['URL_BASE']+'/cas&renew=false',
             'button': 'Sign in using CAS'}
 
 @app.route('/cas')
@@ -18,11 +19,12 @@ def cas_ticket():
 
     alert = None
 
-    u = urllib.request.urlopen(app.config['CAS_SERVER']+'/cas/validate?ticket='+ticket+'&service='+app.config['URL_BASE']+'/cas')
-    response = u.readline().decode('utf-8')
-    if response == 'yes\n':
-        uid = u.readline().decode('utf-8').strip()
-        u.close()
+    u = urllib.request.urlopen(app.config['CAS_SERVER'] + '/cas/serviceValidate?renew=false&ticket=' + ticket + '&service=' + urllib.parse.quote_plus(app.config['URL_BASE']+'/cas'))
+    response = u.read().decode('utf-8')
+    u.close()
+    root = ET.fromstring(response)
+    if root[0].tag == '{http://www.yale.edu/tp/cas}authenticationSuccess':
+        uid = root[0][0].text.strip()
 
         person = storage.session.query(Person).filter_by(stil=uid).scalar()
         if person:
