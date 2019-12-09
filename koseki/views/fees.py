86f48cc66e487e4c0f060fa3d6502192b184c4d8
Foorack <max@foorack.com>
2019-12-09 17:41:47 +0100

    Modularized fees.py view

--- koseki/views/fees.py
+++ koseki/views/fees.py
@@ -1,4 +1,3 @@
-from koseki import app, storage
 from flask import url_for, render_template, session, redirect, escape, request
 from koseki.core import require_session, nav, current_user
 from koseki.db.types import Person, Fee
@@ -11,74 +10,98 @@ from datetime import datetime, timedelta
 import re
 import logging
 
-@app.route('/fees')
-@nav('/fees','certificate','Fees',3,['admin','accounter'])
-@require_session(['admin','accounter'])
-def list_fees():
-    return render_template('list_fees.html', fees=storage.session.query(Fee).order_by(Fee.registered.desc()).all())
 
 class FeeForm(Form):
 
     uid = TextField('Member ID', validators=[DataRequired()])
     amount = DecimalField('Amount (SEK)')
-    method = SelectField('Payment Method', choices=[('swish','Swish'),('cash','Cash'),('bankgiro','Bankgiro'),('creditcard','Credit card')])
+    method = SelectField('Payment Method', choices=[(
+        'swish', 'Swish'), ('cash', 'Cash'), ('bankgiro', 'Bankgiro'), ('creditcard', 'Credit card')])
     retro = DateField('Retroactive fee (Date)', validators=[Optional()])
 
-@app.route('/fees/csv')
-@require_session(['admin','accounter'])
-def export_csv():
-    return render_template('list_fees.csv', fees=storage.session.query(Fee).order_by(Fee.registered.desc()).all())
 
-@app.route('/fees/register', methods=['GET','POST'])
-@require_session(['admin','accounter'])
-def register_fee():
-    form = FeeForm()
+class AddView:
 
-    alerts = []
+    def __init__(self, app, core, storage, mailer):
+        self.app = app
+        self.core = core
+        self.storage = storage
+        self.mailer = mailer
 
-    if form.validate_on_submit():
-        person = storage.session.query(Person).filter_by(uid=form.uid.data).scalar()
+    def register(self):
+        self.app.add_url_rule('/fees', None, self.list_fees)
+        self.app.add_url_rule('/fees/csv', None, self.export_csv)
+        self.app.add_url_rule('/fees/register', None,
+                              self.register_fee, methods=['GET', 'POST'])
+        self.core.nav('/fees', 'certificate', 'Fees',
+                      3, ['admin', 'accounter'])
 
-        if person is None:
-            alerts.append({'class': 'alert-danger',
-                'title': 'Error',
-                'message': 'No such member "%s". Did you try the auto-complete feature?' % form.uid.data})
-            return render_template('register_fee.html', form=form, alerts=alerts)
+    @require_session(['admin', 'accounter'])
+    def list_fees(self):
+        return render_template('list_fees.html', fees=self.storage.session.query(Fee).order_by(Fee.registered.desc()).all())
 
-        if form.retro.data is not None:
-            # Use user-supplied start date
-            start = form.retro.data
-        else:
-            # Calculate period of validity
-            last_fee = storage.session.query(Fee).filter_by(uid=person.uid).order_by(Fee.end.desc()).first()
+    @app.route('/fees/csv')
+    @require_session(['admin', 'accounter'])
+    def export_csv(self):
+        return render_template('list_fees.csv', fees=self.storage.session.query(Fee).order_by(Fee.registered.desc()).all())
 
-            if last_fee and last_fee.end > datetime.now():
-                logging.debug('Last fee: start=%s, end=%s' % (last_fee.start, last_fee.end))
-                start = last_fee.end
+    @require_session(['admin', 'accounter'])
+    def register_fee(self):
+        form = FeeForm()
+
+        alerts = []
+
+        if form.validate_on_submit():
+            person = self.storage.session.query(
+                Person).filter_by(uid=form.uid.data).scalar()
+
+            if person is None:
+                alerts.append({'class': 'alert-danger',
+                               'title': 'Error',
+                               'message': 'No such member "%s". Did you try the auto-complete feature?' % form.uid.data})
+                return render_template('register_fee.html', form=form, alerts=alerts)
+
+            if form.retro.data is not None:
+                # Use user-supplied start date
+                start = form.retro.data
             else:
-                start = datetime.now()
-        #end = start + timedelta(days=(3.65*int(form.amount.data)))
-        end = start + timedelta(days=(1.825*int(form.amount.data)))
+                # Calculate period of validity
+                last_fee = self.storage.session.query(Fee).filter_by(
+                    uid=person.uid).order_by(Fee.end.desc()).first()
 
-        # Store fee
-        fee = Fee(uid=person.uid, registered_by=current_user(), amount=form.amount.data, start=start, end=end, method=form.method.data)
-        storage.add(fee)
-        storage.commit()
+                if last_fee and last_fee.end > datetime.now():
+                    logging.debug('Last fee: start=%s, end=%s' %
+                                  (last_fee.start, last_fee.end))
+                    start = last_fee.end
+                else:
+                    start = datetime.now()
+            #end = start + timedelta(days=(3.65*int(form.amount.data)))
+            end = start + timedelta(days=(1.825*int(form.amount.data)))
 
-        logging.info('Registered %d SEK for %s %s' % (form.amount.data, person.fname, person.lname))
+            # Store fee
+            fee = Fee(uid=person.uid, registered_by=current_user(
+            ), amount=form.amount.data, start=start, end=end, method=form.method.data)
+            self.storage.add(fee)
+            self.storage.commit()
 
-        # Check for user state changes
-        if person.state != 'active' and storage.session.query(Fee).\
-                filter(Fee.uid==person.uid, Fee.start<=datetime.now(), Fee.end>=datetime.now()).all():
-            person.state = 'active'
-            storage.commit()
-            logging.info('%s %s is now active' % (person.fname, person.lname))
-            app.mailer.send_mail(app.config['BOARD_EMAIL'], 'board_member_active.mail', member=person)
-            app.mailer.send_mail(person, 'member_active.mail', member=person)
+            logging.info('Registered %d SEK for %s %s' %
+                         (form.amount.data, person.fname, person.lname))
 
-        alerts.append({'class': 'alert-success',
-            'title': 'Success',
-            'message': 'Registered %d SEK for %s %s' % (form.amount.data, person.fname, person.lname)})
-        form = FeeForm(None)
+            # Check for user state changes
+            if person.state != 'active' and storage.session.query(Fee).\
+                    filter(Fee.uid == person.uid, Fee.start <= datetime.now(), Fee.end >= datetime.now()).all():
+                person.state = 'active'
+                self.storage.commit()
+                logging.info('%s %s is now active' %
+                             (person.fname, person.lname))
+                self.app.mailer.send_mail(
+                    self.app.config['BOARD_EMAIL'], 'board_member_active.mail', member=person)
+                self.app.mailer.send_mail(
+                    person, 'member_active.mail', member=person)
 
-    return render_template('register_fee.html', form=form, alerts=alerts)
+            alerts.append({'class': 'alert-success',
+                           'title': 'Success',
+                           'message': 'Registered %d SEK for %s %s' % (form.amount.data, person.fname, person.lname)})
+            form = FeeForm(None)
+
+        return render_template('register_fee.html', form=form, alerts=alerts)
