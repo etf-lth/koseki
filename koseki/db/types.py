66f14210bf91a7358b59ccb21cde75908f298e82
Foorack <max@foorack.com>
2020-06-13 17:36:08 +0200

    Finished showing "balance" on users

--- koseki/db/types.py
+++ koseki/db/types.py
@@ -1,33 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, Unicode, VARCHAR
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, Unicode, VARCHAR, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import backref, relationship
 
 Base = declarative_base()
-
-
-class Person(Base):
-    __tablename__ = "person"
-
-    uid = Column(Integer, primary_key=True)
-    state = Column(Enum("pending", "active", "expired"), default="pending")
-    fname = Column(Unicode(64))
-    lname = Column(Unicode(64))
-    email = Column(Unicode(64))
-    stil = Column(Unicode(64))
-    password = Column(Unicode(64))
-    enrolled = Column(DateTime, default=datetime.now)
-    lchange = Column(DateTime, default=datetime.now)
-    enrolled_by = Column(Integer, ForeignKey("person.uid"))
-
-    groups = relationship("PersonGroup", backref="person")
-    fees = relationship(
-        "Fee", primaryjoin="Fee.uid==Person.uid", order_by="desc(Fee.fid)"
-    )
-    payments = relationship(
-        "Payment", primaryjoin="Payment.uid==Person.uid", order_by="desc(Payment.pid)"
-    )
 
 class PersonGroup(Base):
     __tablename__ = "person_group"
@@ -69,3 +46,30 @@ class Payment(Base):
     registered = Column(DateTime, default=datetime.now)
     method = Column(Enum("swish", "cash", "bankgiro", "creditcard", "kiosk", "wordpress"), default="swish")
     reason = Column(VARCHAR(length=255))
+
+
+class Person(Base):
+    __tablename__ = "person"
+
+    uid = Column(Integer, primary_key=True)
+    state = Column(Enum("pending", "active", "expired"), default="pending")
+    fname = Column(Unicode(64))
+    lname = Column(Unicode(64))
+    email = Column(Unicode(64))
+    stil = Column(Unicode(64))
+    password = Column(Unicode(64))
+    enrolled = Column(DateTime, default=datetime.now)
+    lchange = Column(DateTime, default=datetime.now)
+    enrolled_by = Column(Integer, ForeignKey("person.uid"))
+
+    groups = relationship("PersonGroup", backref="person")
+    fees = relationship(
+        "Fee", primaryjoin="Fee.uid==Person.uid", order_by="desc(Fee.fid)"
+    )
+    payments = relationship(
+        "Payment", primaryjoin="Payment.uid==Person.uid", order_by="desc(Payment.pid)"
+    )
+
+    @property
+    def balance(self):
+        return sum([p.amount for p in self.payments])
