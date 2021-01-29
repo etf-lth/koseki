89a26ba2f10c1807e870bd2d9b82add27bd01944
Foorack <max@foorack.com>
2021-01-29 17:51:00 +0100

    Fix mail sending to UTF-8 names

--- koseki/mail.py
+++ koseki/mail.py
@@ -1,7 +1,10 @@
 import logging
 import smtplib
 from email.header import Header
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import formataddr
+from typing import Union
 
 from flask import render_template
 
@@ -12,29 +15,40 @@ class KosekiMailer:
     def __init__(self, app):
         self.app = app
 
-    def send_mail(self, to, template, **kwargs):
+    def send_mail(self, to: Union[Person, str], template: str, **kwargs):
         try:
+            msg = MIMEMultipart('alternative')
+
             from_mail = self.app.config["EMAIL_FROM"]
-            if type(to) is Person:
-                to = "%s %s <%s>" % (to.fname, to.lname, to.email)
+            to_mail: str
+
+            if isinstance(to, Person):
+                to_mail = str(to.email)
+                msg["To"] = formataddr(
+                    (str(Header("%s %s" % (to.fname, to.lname), 'utf-8')), to_mail))
+            elif isinstance(to, str):
+                msg["To"] = to_mail = to
+            else:
+                raise TypeError("Mail target was neither string nor Person")
+
+            msg["From"] = Header(from_mail, "utf-8")
+            msg["Subject"] = Header(self.app.config["EMAIL_SUBJECT"], "utf-8")
 
             logging.info(
-                "send_mail to=%s, template=%s, args=%s" % (to, template, kwargs)
+                "send_mail to=%s, template=%s, args=%s" % (
+                    to, template, kwargs)
             )
 
             mimeType = "plain"
             if template.endswith("html"):
                 mimeType = "html"
 
-            msg = MIMEText(
+            msg.attach(MIMEText(
                 render_template(template, **kwargs), mimeType, _charset="utf8"
-            )
-            msg["To"] = Header(to, "utf-8")
-            msg["From"] = Header(from_mail, "utf-8")
-            msg["Subject"] = Header(self.app.config["EMAIL_SUBJECT"], "utf-8")
+            ))
 
             s = smtplib.SMTP(self.app.config["SMTP_SERVER"])
-            s.sendmail(from_mail, [to], msg.as_string())
+            s.sendmail(from_mail, to_mail, msg.as_string())
             s.quit()
         except (Exception, ConnectionRefusedError) as e:
             logging.exception("send_mail failed: %s" % e)
