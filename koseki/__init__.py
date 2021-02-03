3dced16266e734aa890c3a36ba66a517bef7c834
Foorack <max@foorack.com>
2021-02-03 13:42:05 +0100

    Update __init__.py

--- koseki/__init__.py
+++ koseki/__init__.py
@@ -1,18 +1,28 @@
 import logging
+import sys
 
 from koseki.core import KosekiCore
 
-logging.basicConfig(
-    format="%(asctime)s %(message)s",
-    level=logging.DEBUG,
-    handlers=[logging.FileHandler('koseki.log', 'a', 'utf-8')])
+logging_format = "%(asctime)s %(levelname)s %(message)s"
+logging_handler_file = logging.FileHandler('koseki.log', 'a', 'utf-8')
+logging_handler_console = logging.StreamHandler(sys.stdout)
+
 
 def run_prod():
+    logging.basicConfig(
+        format=logging_format,
+        level=logging.DEBUG,
+        handlers=[logging_handler_file])
     core = KosekiCore()
     core.start(flask_server=False)
     return core.app
 
+
 def run_dev():
+    logging.basicConfig(
+        format=logging_format,
+        level=logging.DEBUG,
+        handlers=[logging_handler_file, logging_handler_console])
     core = KosekiCore()
     core.start(flask_server=True)
     return core.app
