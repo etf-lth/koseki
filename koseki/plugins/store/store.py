9436b939d9e1193598138a8bca76765baa53de11
Foorack <max@foorack.com>
2021-02-03 12:14:35 +0100

    Fix Kiosk design after Bootstrap 5 upgrade

--- koseki/plugins/store/store.py
+++ koseki/plugins/store/store.py
@@ -13,8 +13,8 @@ from wtforms.validators import DataRequired  # type: ignore
 class ProductForm(FlaskForm):
     name = TextField("Product name", validators=[DataRequired()])
     img_url = TextField("Image URL", validators=[DataRequired()])
-    price = DecimalField("Price")
-    order = IntegerField("Order")
+    price = DecimalField("Price", validators=[DataRequired()])
+    order = IntegerField("Order", validators=[DataRequired()])
     submitAdd = SubmitField("Add product")
     submitUpdate = SubmitField("Update product")
     submitDelete = SubmitField("Delete product")
@@ -23,7 +23,8 @@ class ProductForm(FlaskForm):
 class StorePlugin(KosekiPlugin):
     def create_blueprint(self) -> Blueprint:
         self.util.nav(
-            "/store", "shopping-basket", "Store", 4, ["admin", "board", "krangare"]
+            "/store", "shopping-basket", "Store", 4, [
+                "admin", "board", "krangare"]
         )
         blueprint: Blueprint = Blueprint(
             "store", __name__, template_folder="./templates"
@@ -47,9 +48,9 @@ class StorePlugin(KosekiPlugin):
         return blueprint
 
     def list_products(self):
-        productForm = ProductForm()
-
         alerts: list[KosekiAlert] = []
+
+        productForm = ProductForm()
 
         if productForm.submitAdd.data and productForm.validate_on_submit():
             # Store product
@@ -63,14 +64,16 @@ class StorePlugin(KosekiPlugin):
             self.storage.commit()
 
             logging.info(
-                "Registered product %s #%d" % (productForm.name.data, product.pid)
+                "Registered product %s #%d" % (
+                    productForm.name.data, product.pid)
             )
 
             alerts.append(
                 KosekiAlert(
                     KosekiAlertType.SUCCESS,
                     "Success",
-                    "Registered product %s #%d" % (productForm.name.data, product.pid),
+                    "Registered product %s #%d" % (
+                        productForm.name.data, product.pid),
                 )
             )
             productForm = ProductForm(None)
@@ -85,10 +88,12 @@ class StorePlugin(KosekiPlugin):
         )
 
     def manage_product(self, pid):
-        productForm = ProductForm()
-        product = self.storage.session.query(Product).filter_by(pid=pid).scalar()
+        product = self.storage.session.query(
+            Product).filter_by(pid=pid).scalar()
         if not product:
             raise abort(404)
+
+        productForm = ProductForm(obj=product)
 
         if productForm.submitDelete.data and productForm.validate_on_submit():
             # Delete product
@@ -98,24 +103,16 @@ class StorePlugin(KosekiPlugin):
             logging.info(
                 "Deleted product %s #%d" % (productForm.name.data, product.pid)
             )
-            return redirect(url_for(self.list_products.__name__))
+            return redirect(url_for("store.list_products"))
 
         if productForm.submitUpdate.data and productForm.validate_on_submit():
             # Update product
-            product.name = productForm.name.data
-            product.img_url = productForm.img_url.data
-            product.price = productForm.price.data
-            product.order = productForm.order.data
+            productForm.populate_obj(product)
             self.storage.commit()
 
             logging.info(
                 "Updated product %s #%d" % (productForm.name.data, product.pid)
             )
-            return redirect(url_for(self.list_products.__name__))
-
-        productForm.name.data = product.name
-        productForm.img_url.data = product.img_url
-        productForm.price.data = product.price
-        productForm.order.data = product.order
+            return redirect(url_for("store.list_products"))
 
         return render_template("store_manage_product.html", form=productForm,)
