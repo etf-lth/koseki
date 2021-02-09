d846a781e87fc57abdd870c69016a9eed9911cd7
Foorack <max@foorack.com>
2021-02-09 22:58:01 +0100

    Emergency fix for CAS auth login

--- koseki/plugin.py
+++ koseki/plugin.py
@@ -43,27 +43,35 @@ class KosekiPluginManager:
 
     def register_plugins(self) -> None:
         plugin: KosekiPlugin
+
+        #
+        # Read config first before enabling
+        #
         for plugin_name in self.app.config["PLUGINS"]:
             logging.info("Registering plugin: %s", plugin_name)
 
+            # Instantiate plugin
             plugin_module: types.ModuleType = importlib.import_module(
                 "koseki.plugins." + plugin_name.lower()
             )
             plugin_type: type = getattr(plugin_module, plugin_name + "Plugin")
-
-            # Instantiate plugin
             plugin = plugin_type(self.app, self.storage, self.auth, self.util)
+            self.plugins[plugin_name] = plugin
+
             # Register config variables
             for key, value in plugin.config().items():
                 self.app.config[key] = value
+
+        # Re-read user config to overwrite/prioritise over plugin config
+        self.app.config.from_pyfile(os.path.join("..", "koseki.cfg"))
+
+        # Enable plugins
+        for plugin_name in self.plugins:
+            plugin = self.plugins[plugin_name]
             # Enable plugin
             plugin.plugin_enable()
             # Register URL handlers
             self.app.register_blueprint(plugin.create_blueprint())
-            self.plugins[plugin_name] = plugin
-
-        # Re-read user config to overwrite/prioritise over plugin config
-        self.app.config.from_pyfile(os.path.join("..", "koseki.cfg"))
 
     def isenabled(self, plugin: str) -> bool:
         return plugin in (p.lower() for p in self.plugins.keys())
