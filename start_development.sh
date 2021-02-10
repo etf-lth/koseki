6d572478a86ffca1f789c94875b769fd3ef66461
ElektroTekniska Föreningen Administratör <info@etf.nu>
2021-02-10 20:06:51 +0000

    Specify Python version

--- start_development.sh
+++ start_development.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.9
 from koseki import run_dev
 run_dev()
--- start_production.sh
+++ start_production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -w 1 --timeout 5 -b 127.0.0.1:5000 "koseki:run_prod()"
+python3.9 -m gunicorn -w 1 --timeout 5 -b 127.0.0.1:5000 "koseki:run_prod()"
