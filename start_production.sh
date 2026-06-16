a27e62af2515d222cfbd4f2a9c25bc9c80c3fe80
Philip Magyar <51290462+Philip2809@users.noreply.github.com>
2026-06-17 00:18:00 +0200

    Update start_production.sh

--- start_production.sh
+++ start_production.sh
@@ -5,4 +5,6 @@ if [ ! -f "signing_key.pem" ]; then
     openssl genrsa -out signing_key.pem 4096
 fi
 
-gunicorn -w 1 --timeout 60 -b 127.0.0.1:5000 "koseki:run_prod()"
+# jag bashar mitt huve i väggen snart
+source .venv/bin/activate
+gunicorn -w 1 --timeout 60 -b 0.0.0.0:5000 "koseki:run_prod()"
